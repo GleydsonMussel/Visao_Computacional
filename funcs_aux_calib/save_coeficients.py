a3e2e87f6b3545072531ed7dd7c3ddc932dd3eba
GleydsonMussel <108836948+GleydsonMussel@users.noreply.github.com>
2023-08-07 22:14:56 -0300

    Limpando Brach

--- funcs_aux_calib/save_coeficients.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def save_coefficients(mtx, dist, new_mtx, path):
-    """ Save the camera matrix and the distortion coefficients to given path/file. """
-    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_APPEND)
-    cv_file.write("M", mtx)
-    cv_file.write("D", dist)
-    cv_file.write("NM", new_mtx)
-    cv_file.release()
